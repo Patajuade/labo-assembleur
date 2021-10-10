499274afb988e7efdd02ee1a99993de66ca0e885
Patajuade <manon.diana@icloud.com>
2021-10-10 19:13:03 +0200

    add améliorations code assembleur

--- Main.asm
+++ Main.asm
@@ -19,11 +19,13 @@
 
 ;************************************************************************
 ;* Définitions et Variables *
-    #DEFINE led1_ON b'00000010'
+    #DEFINE led1_ON b'11111111' ;Define c'est comme déclarer des constantes
     #DEFINE led1_OFF b'00000000'
     #DEFINE Button_RB1_PRESSED  b'00000000'
     #DEFINE Button_RB1_RELEASED b'00000001'
-    
+    ; Quand y'a un # (devant include et define) c'est une directive de 
+    ;précompilation càd que c'est pas le programme qui fait la commande, c'est 
+    ;une commande pour le compilateur
 ;************************************************************************
     cblock 0x020
 ; déclaration de variables
@@ -35,30 +37,28 @@ unevariable,uneautre,temp_1,temp_2,temp_3
 ;************************************************************************
 
 ;    cpu equates (memory map)
-    myPortB    equ    0x06 ;Definit l'addresse du portB
-    myPortA    equ    0x05 ;Definit l'addresse du portA
+    ;myPortB    equ    0x06 ;Definit l'addresse du portB quelque soit la bank 
+			    ;dans laquelle je me trouve j'ai le droit d'utiliser PORTB
+    ;myPortA    equ    0x05 ;Definit l'addresse du portA
     ORG 0x000 ; vecteur reset
 ;************************************************************************
  
 start
     
 ;************************************************************************   
-;Initialisation PortB  
-    BCF STATUS, RP0 ;on clear le bit b dans f
+;Initialisation PortB et PortA p15
+    BCF STATUS, RP0 ;on clear le bit 5 de STATUS, ce qui permet de selectionner bank0
     CLRF PORTB ; initialise portB avec un clear des outputs
-    BSF STATUS, RP0 ; Select Bank 1    
+    CLRF PORTA
+    BSF STATUS, RP0 ; On set le bit 5 de STATUS à 1, donc bank1 est selectionnée    
 ; Partie qui dit que RB7 à RB0 sont des outputs
-    MOVLW 0x00 ;0x00 = output
-    MOVWF TRISB
-
-;Initalisation PortA	p15 et/ou 39
-    BCF STATUS, RP0 ;
-    CLRF PORTA ; initialise portB avec un clear des outputs
-    BSF STATUS, RP0 ; Select Bank 1 
-;Partie qui dit que RA1 est un input
+    MOVLW 0x00 ;0x00 = hexa / b'0' = binaire / 0 = decimal on doit préciser le système de numération
+    MOVWF TRISB ; on met 0x00 dans TRISB, ce qui met PORTB en output
+;Partie qui dit que RA4 à RA0 sont des inputs
     MOVLW 0x01 ;0x01 = input
     MOVWF TRISA ;on met 1 dans trisA : input
     
+    BCF STATUS, RP0 ;On repasse dans la bank0 pour pouvoir utiliser PORTA et B sans utiliser les trucs à la ligne 40
 ;************************************************************************
 ;************************************************************************ 
 MAIN
@@ -68,13 +68,13 @@ MAIN
     
 sub_led1_on
     movlw led1_ON    ; move led1_ON dans W
-    movwf myPortB        ; move W dans f (ça bouge ce qu'y a dans w dans portB)
+    movwf PORTB        ; move W dans f (ça bouge ce qu'y a dans w dans portB)
     call Delay
     return
     
 sub_led1_off
     movlw led1_OFF ; move led1_OFF dans w
-    movwf myPortB        ; move W dans f (ça bouge ce qu'y a dans w dans portB)
+    movwf PORTB        ; move W dans f (ça bouge ce qu'y a dans w dans portB)
     call Delay
     return
     
