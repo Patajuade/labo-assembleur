fe6ce8f5a9191b327dac1bce77b723c517ebf9a2
Patajuade <manon.diana@icloud.com>
2021-10-13 21:34:25 +0200

    add stop feature on ra0 button

--- Labo/Labo1_1led_1bouton_onoff.asm
+++ Labo/Labo1_1led_1bouton_onoff.asm
@@ -5,31 +5,31 @@
 ;* Auteur:  *
 ;* Haute Ecole Louvain en Hainaut *
 ;************************************************************************
-;* Fichiers nécessaires: aucun *
+;* Fichiers nÃ©cessaires: aucun *
 ;************************************************************************
 ;* Notes: *
 ;************************************************************************
     list p=16F84, f=INHX8M ; directive pour definir le processeur
     list c=90, n=60 ; directives pour le listing
-    #include <p16F84a.inc> ; incorporation variables spécifiques
+    #include <p16F84a.inc> ; incorporation variables spÃ©cifiques
     errorlevel -302 ; pas d'avertissements de bank
     errorlevel -305 ; pas d'avertissements de fdest
 
     __config _CP_OFF & _WDT_OFF & _PWRTE_ON & _HS_OSC
 
 ;************************************************************************
-;* Définitions et Variables *
-    #DEFINE led1_ON b'11111111' ;Define c'est comme déclarer des constantes
+;* DÃ©finitions et Variables *
+    #DEFINE led1_ON b'11111111' ;Define c'est comme dÃ©clarer des constantes
     #DEFINE led1_OFF b'00000000'
     #DEFINE Button_RB1_PRESSED  b'00000000'
     #DEFINE Button_RB1_RELEASED b'00000001'
     ; Quand y'a un # (devant include et define) c'est une directive de 
-    ;précompilation càd que c'est pas le programme qui fait la commande, c'est 
+    ;prÃ©compilation cÃ d que c'est pas le programme qui fait la commande, c'est 
     ;une commande pour le compilateur
 ;************************************************************************
     cblock 0x020
-; déclaration de variables
-unevariable,uneautre,temp_1,temp_2,temp_3,IS_LED_BLINKING
+; dÃ©claration de variables
+unevariable,uneautre,temp_1,temp_2,temp_3
     endc   
  ;equ
 ;************************************************************************
@@ -50,70 +50,48 @@ start
     BCF STATUS, RP0	    ;on clear le bit 5 de STATUS, ce qui permet de selectionner bank0
     CLRF PORTB		    ; initialise portB avec un clear des outputs
     CLRF PORTA
-    BSF STATUS, RP0	    ; On set le bit 5 de STATUS à 1, donc bank1 est selectionnée    
-; Partie qui dit que RB7 à RB0 sont des outputs
-    MOVLW 0x00		    ;0x00 = hexa / b'0' = binaire / 0 = decimal on doit préciser le système de numération
+    BSF STATUS, RP0	    ; On set le bit 5 de STATUS Ã  1, donc bank1 est selectionnÃ©e    
+; Partie qui dit que RB7 Ã  RB0 sont des outputs
+    MOVLW 0x00		    ;0x00 = hexa / b'0' = binaire / 0 = decimal on doit prÃ©ciser le systÃ¨me de numÃ©ration
     MOVWF TRISB		    ; on met 0x00 dans TRISB, ce qui met PORTB en output
-;Partie qui dit que RA4 à RA0 sont des inputs
+;Partie qui dit que RA4 Ã  RA0 sont des inputs
     MOVLW b'00000111'	    ;0x01 = input
     MOVWF TRISA		    ;on met 1 dans trisA : input
     
-    BCF STATUS, RP0	    ;On repasse dans la bank0 pour pouvoir utiliser PORTA et B sans utiliser les trucs à la ligne 40
+    BCF STATUS, RP0	    ;On repasse dans la bank0 pour pouvoir utiliser PORTA et B sans utiliser les trucs Ã  la ligne 40
 ;************************************************************************
 ;************************************************************************ 
 MAIN
-    CALL CHECK_RA0
-    BTFSC IS_LED_BLINKING, 0
-    CALL BLINK_ALL_LEDS_ONCE
+    CALL CHECK_RA0 
     GOTO MAIN
     
-CHECK_RA0		;Subroutine qui permet d'allumer/éteindre la led
-    BTFSS PORTA,RA0	;On teste les bits qu'on a dans f (donc PORTA), s'ils sont =1 on skip l'instruction suivante immédiate et on va à celle d'après
-    RETURN		;Return execute l'instruction après le call de la fonction CHECK_RA0
-			;Donc là si les bits PORTA = 0 on vient ici, si les bits PORTA = 1 on la saute
-    ;CALL DELAY_100MS	;Delay de 100ms pour éviter les rebonds du bouton
-    BTFSC IS_LED_BLINKING,0	;
-    GOTO LED_OFF	;
+CHECK_RA0		;Subroutine qui permet d'allumer/Ã©teindre la led
+    BTFSS PORTA,RA0	;On teste les bits qu'on a dans f (donc PORTA), s'ils sont =1 on skip l'instruction suivante immÃ©diate et on va Ã  celle d'aprÃ¨s
+    RETURN		;Return execute l'instruction aprÃ¨s le call de la fonction CHECK_RA0
+			;Donc lÃ  si les bits PORTA = 0 on vient ici, si les bits PORTA = 1 on la saute
+    ;CALL DELAY_100MS	;Delay de 100ms pour Ã©viter les rebonds du bouton
+    BTFSC PORTB,RB0	;si les bits de PORTB=0 on skip l'instruction suivante immÃ©diate (donc si la led n'est pas allumÃ©e)
+    GOTO LED_OFF	;D'aprÃ¨s l'instruction au dessus, si les bits de PORTB =0 on skip ici, sinon on skip pas et la led s'Ã©teint
 LED_ON
-    BTFSC PORTA,RA0	;si les bits de PORTA=0 on skip l'instruction suivante immédiate (le bouton n'est pas appuyé donc on allume pas les leds)
-    GOTO LED_ON		;on va ici que si les bits de PORTA=1 (donc si le bouton est appuyé)
-    MOVLW 0x01
-    MOVWF IS_LED_BLINKING
-    ;BSF PORTB,RB0	;on set les bits de PORTB à 1 -> les leds s'allument
-    GOTO MAIN
+    BTFSC PORTA,RA0	;si les bits de PORTA=0 on skip l'instruction suivante immÃ©diate (le bouton n'est pas appuyÃ© donc on allume pas les leds)
+    GOTO LED_ON		;on va ici que si les bits de PORTA=1 (donc si le bouton est appuyÃ©)
+    BSF PORTB,RB0	;on set les bits de PORTB Ã  1 -> les leds s'allument
+    RETURN
 LED_OFF
-    BTFSC PORTA,RA0	;si les bits de PORTA=0 on skip l'instruction suivante immédiate (le bouton n'est pas appuyé donc on allume pas les leds)
-    GOTO LED_OFF	;on va ici que si les bits de PORTA=1 (donc si le bouton est appuyé)
-    MOVLW 0x00
-    MOVWF IS_LED_BLINKING
-    movlw led1_OFF	; move led1_OFF dans w
-    movwf PORTB		; move W dans f (ça bouge led1_OFF dans w dans portB)
-    ;BCF PORTB,RB0	;on clear les bits de PORTB  -> les leds s'éteignent
-    GOTO MAIN
-
-
-BLINK_ALL_LEDS_ONCE
+    BTFSC PORTA,RA0	;si les bits de PORTA=0 on skip l'instruction suivante immÃ©diate (le bouton n'est pas appuyÃ© donc on allume pas les leds)
+    GOTO LED_OFF	;on va ici que si les bits de PORTA=1 (donc si le bouton est appuyÃ©)
+    BCF PORTB,RB0	;on clear les bits de PORTB  -> les leds s'Ã©teignent
+    RETURN
+    
+BLINK_ALL_LEDS
     movlw led1_ON	; move led1_ON dans le W
-    movwf PORTB		; move W dans f (ça bouge led1_ON dans w dans portB)
-    call DELAY_WITH_CHECK_BUTTON
+    movwf PORTB		; move W dans f (Ã§a bouge led1_ON dans w dans portB)
+    call DELAY_1S
     movlw led1_OFF	; move led1_OFF dans w
-    movwf PORTB		; move W dans f (ça bouge led1_OFF dans w dans portB)
-    call DELAY_WITH_CHECK_BUTTON
+    movwf PORTB		; move W dans f (Ã§a bouge led1_OFF dans w dans portB)
+    call DELAY_1S
     return
     
-DELAY_WITH_CHECK_BUTTON
-    CALL DELAY_100MS
-    CALL CHECK_RA0
-    CALL DELAY_100MS
-    CALL CHECK_RA0
-    CALL DELAY_100MS
-    CALL CHECK_RA0
-    CALL DELAY_100MS
-    CALL CHECK_RA0
-    CALL DELAY_100MS
-    CALL CHECK_RA0
-    CALL DELAY_100MS
-    CALL CHECK_RA0
 ;------------------------------- Delay -----------------------------------
 	cblock
 	d1
